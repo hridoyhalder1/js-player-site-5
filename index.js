adb5a01dedc5245b8b12d35dc394a56258b40b33
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-08-22 23:47:26 +0600

    calculte

--- index.js
+++ index.js
@@ -1,20 +1,72 @@
 document.getElementById('button-1').addEventListener('click', function(){
     const lionelMessi = document.getElementById('lionel-messi');
     lionelMessi.style.display = 'block';
+    document.getElementById('button-1').disabled = true;
+   
 })
 document.getElementById('button-2').addEventListener('click', function(){
-    const lionelMessi = document.getElementById('neymar-jr');
-    lionelMessi.style.display = 'block';
+    const neymarJr = document.getElementById('neymar-jr');
+    neymarJr.style.display = 'block';
 })
 document.getElementById('button-3').addEventListener('click', function(){
-    const lionelMessi = document.getElementById('kylian-mbappe');
-    lionelMessi.style.display = 'block';
+    const kylianMbappe = document.getElementById('kylian-mbappe');
+    kylianMbappe.style.display = 'block';
 })
 document.getElementById('button-4').addEventListener('click', function(){
-    const lionelMessi = document.getElementById('vítor-machado');
-    lionelMessi.style.display = 'block';
+    const vitorMachado = document.getElementById('vítor-machado');
+    vitorMachado.style.display = 'block';
 })
 document.getElementById('button-5').addEventListener('click', function(){
-    const lionelMessi = document.getElementById('sergio-ramos');
-    lionelMessi.style.display = 'block';
+    const sergioRamos = document.getElementById('sergio-ramos');
+    sergioRamos.style.display = 'block';
+})
+
+// budget calculation
+document.getElementById('calculate').addEventListener('click', function(){
+    const perPlayerField = document.getElementById('per-player-field');
+    const perPlayerFieldString = perPlayerField.value;
+    const previousPerPlayerNumber = parseFloat(perPlayerFieldString);
+
+    const previousPlayerExpenses = previousPerPlayerNumber * 5;
+    perPlayerField.value = previousPlayerExpenses;
+    
+
+    const playerExpenses = document.getElementById('player-expenses');
+    const playerExpensesString = playerExpenses.innerText;
+    const totalPlayerExpenses = parseFloat(playerExpensesString);
+
+    const newTotalPlayerExpenses = previousPlayerExpenses ;
+    playerExpenses.innerText = newTotalPlayerExpenses;
+    perPlayerField.value = '';
+
+    // console.log(previousPerPlayerNumber);
+    // perPlayerField.value = previousPerPlayerNumber;
+    
+})
+document.getElementById('calculate-total').addEventListener('click', function(){
+    
+    
+    const playerExpenses = document.getElementById('player-expenses');
+    const playerExpensesString = playerExpenses.innerText;
+    const totalPlayerExpenses = parseFloat(playerExpensesString);
+
+    const managerExpense = document.getElementById('manager-expense');
+    const managerExpenseString = managerExpense.value;
+    const managerExpenseNumber = parseInt(managerExpenseString);
+    // console.log(managerExpenseNumber);
+
+    const coachExpense = document.getElementById('coach-expense');
+    const coachExpenseString = coachExpense.value;
+    const coachExpenseNumber = parseInt(coachExpenseString);
+    // console.log(coachExpenseNumber);
+
+    
+    // console.log(calculateTotal);
+    const totalExpense = document.getElementById('total-expense');
+    const totalExpenseString = totalExpense.innerText;
+    const totalExpenseNumber = parseInt(totalExpenseString);
+
+    const calculateTotal = totalPlayerExpenses + managerExpenseNumber + coachExpenseNumber ;
+    totalExpense.innerText = calculateTotal;
+    
 })
