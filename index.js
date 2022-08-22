d3315727b7f94e3e5b24ef457005a842a7856d57
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-08-22 13:44:50 +0600

    selectedv

---
+++ index.js
@@ -0,0 +1,20 @@
+document.getElementById('button-1').addEventListener('click', function(){
+    const lionelMessi = document.getElementById('lionel-messi');
+    lionelMessi.style.display = 'block';
+})
+document.getElementById('button-2').addEventListener('click', function(){
+    const lionelMessi = document.getElementById('neymar-jr');
+    lionelMessi.style.display = 'block';
+})
+document.getElementById('button-3').addEventListener('click', function(){
+    const lionelMessi = document.getElementById('kylian-mbappe');
+    lionelMessi.style.display = 'block';
+})
+document.getElementById('button-4').addEventListener('click', function(){
+    const lionelMessi = document.getElementById('vítor-machado');
+    lionelMessi.style.display = 'block';
+})
+document.getElementById('button-5').addEventListener('click', function(){
+    const lionelMessi = document.getElementById('sergio-ramos');
+    lionelMessi.style.display = 'block';
+})
